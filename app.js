e450636be13dfe08642fe280ad84ffc26908069b
MassMatsu <masa10matsukawa@gmail.com>
2021-05-31 10:48:16 +0900

    modified css

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ let answer = getRandomNum();
 let currentScore = 20;
 let highest = 0;
 let guess = 10;
-console.log('guess', guess)
+
 
 // set values and display
 initializeValues(currentScore, highest)
-
-//console.log(document.querySelector('.guess').value)
 
 // when click check button
 document.querySelector('.check').addEventListener('click', () => {
@@ -46,6 +44,8 @@ document.querySelector('.again').addEventListener('click', () => {
 
 });
 
+
+// -----------------------------------------------------
 // click to increase or decrease guess number (it is for mobile phone)
 document.querySelector('.plusBtn').addEventListener('click', () => {
   guess ++
